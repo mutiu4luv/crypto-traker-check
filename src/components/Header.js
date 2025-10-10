42bd63b408271e1549937de500dbf188e4f1d3f1
mutiu4luv <chidiemmamadu@gmail.com>
2025-10-10 03:55:49 +0100

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -49,7 +49,7 @@ function Header() {
               variant="h6"
               className={classes.title}
             >
-              Crypto Hunter
+              Mutiu Crypto Hunter
             </Typography>
             {/* <Button color="inherit">Login</Button> */}
             <Select
